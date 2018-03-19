5fd61ab082a0a7b27048dd0ba34d36f1843b860c
Saddam Hossen <37399627+NidsML@users.noreply.github.com>
2018-03-19 11:51:09 +0100

    Create Classifiers.py

---
+++ Classifiers.py
@@ -0,0 +1,13 @@
+import gym
+from sklearn.tree import DecisionTreeClassifier
+
+class Classififer:
+    def __init__(self):
+        pass
+
+    def get_classifier(self):
+        '''
+            returns Classifier object
+        '''
+        clf = SGDClassifier()
+return clf
